eb9ba20a8cb82eebc4fb0b7d137126ae311cbc38
Ian Crisp <iancrisp@program-systems.co.uk>
2019-05-03 11:31:13 +0100

    Initial commit

---
+++ DFC.Composite.Regions/Functions/PatchRegionHttpTrigger.cs
@@ -0,0 +1,118 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using DFC.Common.Standard.Logging;
+using DFC.Functions.DI.Standard.Attributes;
+using DFC.HTTP.Standard;
+using DFC.JSON.Standard;
+using DFC.Swagger.Standard.Annotations;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using static DFC.Composite.Regions.Models.Constants;
+
+namespace DFC.Composite.Regions.Functions
+{
+    public static class PatchRegionHttpTrigger
+    {
+        [FunctionName("Patch")]
+        [ProducesResponseType(typeof(Models.Region), (int)HttpStatusCode.OK)]
+        [Response(HttpStatusCode = (int)HttpStatusCode.OK, Description = "Region found", ShowSchema = true)]
+        [Response(HttpStatusCode = (int)HttpStatusCode.NoContent, Description = "Region does not exist", ShowSchema = false)]
+        [Response(HttpStatusCode = (int)HttpStatusCode.BadRequest, Description = "Request was malformed", ShowSchema = false)]
+        [Response(HttpStatusCode = (int)HttpStatusCode.UnprocessableEntity, Description = "Region validation error(s)", ShowSchema = false)]
+        [Display(Name = "Patch", Description = "Ability to update specific values of an existing Region for a Path. <br>" +
+                                             "<br><b>Validation Rules:</b> <br>" +
+                                             "<br><b>Path:</b> Is mandatory <br>" +
+                                             "<br><b>PageRegion:</b> Is mandatory <br>" +
+                                             "<br><b>RegionEndpoint:</b> Is mandatory <br>"
+            )]
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "patch", Route = "paths/{path}/regions/{pageRegion}")]
+            HttpRequest req,
+            ILogger log,
+            string path,
+            int pageRegion,
+            [Inject] ILoggerHelper loggerHelper,
+            [Inject] IHttpRequestHelper httpRequestHelper,
+            [Inject] IHttpResponseMessageHelper httpResponseMessageHelper,
+            [Inject] IJsonHelper jsonHelper
+        )
+        {
+            loggerHelper.LogMethodEnter(log);
+
+            // validate the parameters are present
+            var dssCorrelationId = httpRequestHelper.GetDssCorrelationId(req);
+            if (string.IsNullOrEmpty(dssCorrelationId))
+            {
+                log.LogInformation($"Unable to locate '{nameof(dssCorrelationId)}' in request header");
+            }
+
+            if (!Guid.TryParse(dssCorrelationId, out var correlationGuid))
+            {
+                log.LogInformation($"Unable to parse '{nameof(dssCorrelationId)}' to a Guid");
+                correlationGuid = Guid.NewGuid();
+            }
+
+            if (string.IsNullOrEmpty(path))
+            {
+                loggerHelper.LogInformationMessage(log, correlationGuid, $"Missing value in request for '{nameof(path)}");
+                return httpResponseMessageHelper.BadRequest();
+            }
+
+            if (pageRegion == 0)
+            {
+                loggerHelper.LogInformationMessage(log, correlationGuid, $"Missing value in request for '{nameof(pageRegion)}");
+                return httpResponseMessageHelper.BadRequest();
+            }
+
+            Models.Region regionRequest;
+
+            try
+            {
+                loggerHelper.LogInformationMessage(log, correlationGuid, "Attempt to get resource from body of the request");
+                regionRequest = await httpRequestHelper.GetResourceFromRequest<Models.Region>(req);
+            }
+            catch (JsonException ex)
+            {
+                loggerHelper.LogError(log, correlationGuid, "Unable to retrieve body from req", ex);
+                return httpResponseMessageHelper.UnprocessableEntity(ex);
+            }
+
+
+
+
+
+            //////////////////////////////////////
+            // sample code - to delete vvvvvvvvvvv
+            //////////////////////////////////////
+
+
+
+
+            var region = regionRequest;
+            region.PageRegion = pageRegion;
+
+
+            //////////////////////////////////////
+            // sample code - to delete ^^^^^^^^^^^
+            //////////////////////////////////////
+
+
+
+
+
+
+            loggerHelper.LogMethodExit(log);
+
+            return region != null
+                ? httpResponseMessageHelper.Ok(jsonHelper.SerializeObjectAndRenameIdProperty(region, "id", nameof(Models.Region.DocumentId)))
+                : httpResponseMessageHelper.NoContent();
+        }
+    }
+}
