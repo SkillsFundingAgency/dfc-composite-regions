b7a0f1c71dda2b90dc5ca27fda48ad6e8498cd22
Ian Crisp <icrisp@program-systems.co.uk>
2019-06-04 09:13:45 +0100

    Updated names of environment variables following DevOps deployment

--- DFC.Composite.Regions/Models/EnvironmentVariableNames.cs
+++ DFC.Composite.Regions/Models/EnvironmentVariableNames.cs
@@ -6,9 +6,9 @@ namespace DFC.Composite.Regions.Models
 {
     public static class EnvironmentVariableNames
     {
-        public const string CosmosConnectionString = "CosmosConnectionString";
-        public const string CosmosDatabaseId = "CosmosDatabaseId";
-        public const string CosmosCollectionId = "CosmosCollectionId";
-        public const string CosmosPartitionKey = "CosmosPartitionKey";
+        public const string CosmosConnectionString = "CosmosSettings__ConnectionString";
+        public const string CosmosDatabaseId = "CosmosSettings__DatabaseId";
+        public const string CosmosCollectionId = "CosmosSettings__CollectionId";
+        public const string CosmosPartitionKey = "CosmosSettings__PartitionKey";
     }
 }
